b0db27680940e242e7a4e07e87f655301ef7c7d7
xiazihang <xiazh@sudiyi.net>
2016-12-03 23:08:47 +0800

    move require controller

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,5 @@
 require 'Rulers'
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", "app", "controllers"))
-require 'api_controller'
-require 'home_controller'
 
 module BestQuotes
   class Application < Rulers::Application
